a928972cff309b9253ab19ae0b103abefdd14045
Youssef0Eldeeb <youssefeldeeb3109@gmail.com>
2023-04-12 17:34:27 +0200

    Initial Commit

---
+++ CarsReview/CarsReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsReviewApp.swift
+//  CarsReview
+//
+//  Created by Youssef Eldeeb on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarsReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
